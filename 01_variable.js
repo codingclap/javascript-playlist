717aaa0cb7a71b5ca6ea96b03d4e4319a0465194
codingclap <codingclap@gmail.com>
2024-09-09 18:25:33 +0530

    variable

---
+++ 01_variable.js
@@ -0,0 +1,19 @@
+'use-strict'
+
+let userName = "amit";
+
+let userPassword = 123;
+
+const userState = false;
+
+let userAddress;                 // returned Undefined value because we are not asssign any value here
+
+// let userPassword = 'abcd123';   // Can not Re-declared
+
+//userState = true;        // Can not overwrite
+
+
+console.table([userName, userPassword, userState, userAddress]);
+
+
+
