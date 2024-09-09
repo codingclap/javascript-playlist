31ca9b4fcf935b44562b82fcced7b4aa4d4fbfaa
codingclap <codingclap@gmail.com>
2024-09-09 18:26:17 +0530

    datatype

---
+++ 02_dataType.js
@@ -0,0 +1,16 @@
+'use-strict'
+
+let name = "anuj kumar";         // DataType String
+
+let age = 33;                    // DataType number
+  
+let status = true;              // DataType Boolean
+
+let money = 333.2;               // DataType number
+
+let arrr = [];                   // DataType Object
+ 
+console.log(typeof null);        // Object
+
+console.log(typeof undefined);   // Undefined
+
