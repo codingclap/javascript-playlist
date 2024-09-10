d2abe1729fc05e68ed9e0efa51e5025bcfdfc14c
codingclap <codingclap@gmail.com>
2024-09-10 16:08:08 +0530

    Datatype

---
+++ 03_datatype_summary.js
@@ -0,0 +1,36 @@
+// Primitive  -  Stack Memory
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+let name = "amit kumar"
+let age = 223
+let isMale = true
+let wetherTemp = null
+
+const password = Symbol('123');
+const newPassword = Symbol('123');
+const bigInt = 18876567852333n;
+
+// console.log(typeof(password));                    // Return Symbol
+// console.log(typeof(wetherTemp));                    // Return object
+// console.log(typeof(bigInt));                          // Return Big Int DataType
+// console.log(password === newPassword);                   // Return Diffrent DataType
+// console.table([wetherTemp, password, newPassword]);      // Return in a Tabular Formate
+
+
+
+
+// Reference (Non Primitive)   - Heap Memory
+
+// Array, Object, Function
+
+
+
+
+
+
+
+
+
+
+
+
