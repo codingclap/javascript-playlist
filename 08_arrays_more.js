b4dc9f679144f2c94fcbf9ccd87907a0146d9b22
codingclap <codingclap@gmail.com>
2024-09-15 21:56:42 +0530

    arrow second

---
+++ 08_arrays_more.js
@@ -0,0 +1,11 @@
+let heros_one = ['spiderman','batman','superman']
+let hero_two = ['mangoman','antman','madamweb']
+
+
+//Four way to convert number into a string
+
+toString(num)
+new String(num)
+`${num}`
+
+JSON.stringift(num)
