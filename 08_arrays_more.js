65ae5dd0f8c39426e6b14810c2493987fc7082b5
codingclap <codingclap@gmail.com>
2024-09-19 15:29:35 +0530

    array questions

--- 08_arrays_more.js
+++ 08_arrays_more.js
@@ -1,5 +1,5 @@
-let hero_one = ['spiderman','batman','superman']
-let hero_two = ['mangoman','antman','madamweb']
+let hero_one = ['spiderman', 'batman', 'superman']
+let hero_two = ['mangoman', 'antman', 'madamweb']
 
 // +++++++++++++  Push and concat Method ++++++++++
 
@@ -19,7 +19,7 @@ let all_hero3 = [...hero_one, ...hero_two];     // spread operator return new ar
 
 // ++++++++++++ Flat +++++++++++++++
 
-let india = ['people','summery',[12,33,55,],['manoj', 'sandeep', [1991, 2024, 1023]]]
+let india = ['people', 'summery', [12, 33, 55,], ['manoj', 'sandeep', [1991, 2024, 1023]]]
 
 let newIndaia = india.flat(2)         // Return a new array with all the nested array
 
@@ -34,15 +34,60 @@ let newPop = "anuj kumar";
 //console.log(Array.isArray('newPop'));           // Check and return false if array not found
 //console.log(Array.isArray(['newPop']));          // Check and return True if array found
 
-let popString = "sandeep, deepak";       
+let popString = "sandeep, deepak";
 let popArray = [1, 2, 3];
 //console.log(Array.from(popString))  //Its convert string into the new array with iterable(loop)
-//console.log(Array.from(popArray))  //Its convert array into the new array with iterable(loop)
+//console.log(Array.from(popArray))  //Its convert any array into the new shallow copy with iterable(loop)
 
 
 
-let ponyTail = "sandeep, pankaj, mannu";
+let ponyTail1 = "sandeep";
+let ponyTail2 = "Anuj Kumar";
+let ponyTail3 = "Kuldeep Singh";
 
-let newPony = Array.of(ponyTail);  // Convert string, numbers into the array
-console.log(newPony[1])
- 
+let newPony = Array.of(ponyTail1, ponyTail2, ponyTail3);  // Convert string, numbers into the array
+//console.log(newPony)
+
+
+
+
+//++++++++++++++++++++++++++++++  Examples ++++++++++++++++++++++++++++++++
+
+let student = "AAABBCCCDEEEFFFFF";
+
+
+// Ex - 1     ----------    Remove Duplicate character from string
+
+let pops = "";
+for (let char of student) {
+    if (!pops.includes(char)) {
+        pops += char
+    }
+}
+//console.log(pops);              // Output: ABCDEF
+
+
+
+// EX - 2    ----------     Filter and Add the characters that appear more than Zero.
+
+let teacher = "anuj kumar deepak";
+let sortChar = {};
+
+for (let char of teacher) {
+    //console.log(sortChar[char])
+    if(sortChar[char] === undefined){
+        sortChar[char] = 1
+    }else{
+        sortChar[char] += 1
+    }
+    
+}
+
+let sortTeacher = [];
+for (let chars in sortChar) {
+
+    if (sortChar[chars] > 0) {
+        sortTeacher.push(`${sortChar[chars]}${chars}`)
+    }
+}
+console.log(sortTeacher)          // Output: ['6A', '2B','3C', '1c','1D', '3E','5F']
