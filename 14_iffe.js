e08bda50e096e2bf53a96713f5418d8e29ac8f49
codingclap <codingclap@gmail.com>
2024-09-26 16:24:41 +0530

    iffe chnages

--- 14_iffe.js
+++ 14_iffe.js
@@ -1,14 +1,15 @@
 // Immediately Invoked Function Expression
 
 
-// Using Function Declaration
+// Named IFFE
 (function daily(h, w){   
+
   console.log(`Function Declaration Output = ${h * w}`)
 })(2, 5);
 
 
 
-// Using arrow function
+// Un-Named IFFE
 
 ( (h, w)=>{
   console.log(`Arrow Function Output = ${h * w}`)
