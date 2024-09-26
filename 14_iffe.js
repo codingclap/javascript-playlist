9f8148fe5484fbafb688a4364600b08d00a4c0b5
codingclap <codingclap@gmail.com>
2024-09-26 16:23:27 +0530

    iffe invoked function

---
+++ 14_iffe.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expression
+
+
+// Using Function Declaration
+(function daily(h, w){   
+  console.log(`Function Declaration Output = ${h * w}`)
+})(2, 5);
+
+
+
+// Using arrow function
+
+( (h, w)=>{
+  console.log(`Arrow Function Output = ${h * w}`)
+} )(6, 7)
+
