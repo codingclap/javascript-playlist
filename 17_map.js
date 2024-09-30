1643dc9d14b04149d91fa18577a8b116365cb70c
codingclap <codingclap@gmail.com>
2024-09-30 11:06:33 +0530

    maps First draft

---
+++ 17_map.js
@@ -0,0 +1,25 @@
+// Maps
+
+const puppy = new Map();
+//console.log(typeof puppy);        // It's a Object
+
+
+puppy.set('DogName', 'newDog')
+puppy.set('DogName', 'montiDog')
+puppy.set('DogAge', 23)
+puppy.set('DogColor', 'red')
+
+console.log(puppy);      // Return a object after checking/Removing duplicate values { 'DogName' => 'montiDog','DogAge' => '23', 'DogColor' => 'red' }
+
+//------
+
+for(const [index, value] of puppy){                  // De-Structuring the object
+    console.log(`${index} :- ${value}`)
+}
+
+//------
+
+
+//XXXX----   Map Object is not iteratable thought (For - In) Loop.  ----XXXX
+ 
+// for(const [index, value] in puppy){ }
