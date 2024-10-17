d75e85818763a56c7a9197190eefb657d1f7e54b
codingclap <codingclap@gmail.com>
2024-10-17 17:14:54 +0530

    constructor

--- 26_oops.js
+++ 26_oops.js
@@ -29,15 +29,18 @@ function UserName(username, loginCount, isLoggedIn) {
     return this
 }
 
+
 //Wrong way
 // const userOne = UserName("Hidamba", 123, true);
 // const userTwo = UserName("Anuj kumar", 112, true);
 
 
 //Correct way
-  const userOne = new UserName("Hidamba", 123, true);
-  const userTwo = new UserName("Anuj kumar", 112, true);
+const userOne = new UserName("Hidamba", 123, true);
+const userTwo = new UserName("Anuj kumar", 112, true);
 
+console.log(userOne instanceof UserName);               // Check whather userOne is a instance of UserName Function
+// Expected output: true
 
-console.log(userOne);                                      
+console.log(userOne);
 console.log(userTwo);                                    
